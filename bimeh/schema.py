7e4471d1889c7e4720b128b8cd6e3a12cc5511da
Mohammad <darbanhandrew@gmail.com>
2021-12-19 13:27:49 +0330

    Added django graphene plus and django graphql jwt

---
+++ bimeh/schema.py
@@ -0,0 +1,13 @@
+import graphene
+from userprofile.schema import UserQuery, UserMutation
+
+
+class Query(UserQuery, graphene.ObjectType):
+    pass
+
+
+class Mutation(UserMutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
