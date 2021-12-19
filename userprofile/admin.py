2dc654090540df861662c15e9135bdcf7f5e6a0f
Mohammad <darbanhandrew@gmail.com>
2021-12-19 11:39:26 +0330

    Changed user admin list_display and removed is_staff

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    list_display = ('username', 'email', 'first_name', 'last_name',)
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
     search_fields = ('username', 'first_name', 'last_name', 'email')
     ordering = ('username',)
